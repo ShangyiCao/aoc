e45480e8f04bc925b46ffb59b39d1879f042520b
Shangyi Cao <shacao@ethz.ch>
2025-12-12 17:42:28 +0100

    add Set_remove

--- include/set.h
+++ include/set.h
@@ -7,100 +7,124 @@
 #include "hash.h"
 
 #define LOAD_FACTOR 0.75
+#define DELETED ((void *)-1)
 
 typedef struct Set {
-    size_t sizeOfData;
-    size_t len;
-    size_t capacity;
-    void **table;
+  size_t sizeOfData;
+  size_t len;
+  size_t capacity;
+  void **table;
 } Set;
 
 Set *Set_create(size_t capacity, size_t sizeOfData) {
-    Set *set = calloc(1, sizeof(Set));
-    assert(set);
-    set->sizeOfData = sizeOfData;
-    set->len = 0;
-    assert(capacity);
-    set->capacity = capacity;
-    set->table = calloc(set->capacity, sizeof(void *));
-    return set;
+  Set *set = calloc(1, sizeof(Set));
+  assert(set);
+  set->sizeOfData = sizeOfData;
+  set->len = 0;
+  assert(capacity);
+  set->capacity = capacity;
+  set->table = calloc(set->capacity, sizeof(void *));
+  return set;
 }
 
 void Set_add(Set *set, void *data) {
-    assert(set);
-    if (set->len >= set->capacity * LOAD_FACTOR) {
-        printf("Warning: rehashing set!\n");
-        size_t capacity = set->capacity * 2;
-        void **table = calloc(capacity, sizeof(void *));
-        for (int i = 0; i < set->capacity; i++) {
-            if (set->table[i]) {
-                size_t index = hash(set->table[i], set->sizeOfData, capacity);
-                while (table[index]) {
-                    index = (index + 1) % capacity;
-                }
-                table[index] = set->table[i];
-            }
+  assert(set);
+  if (set->len >= set->capacity * LOAD_FACTOR) {
+    printf("Warning: rehashing set!\n");
+    size_t capacity = set->capacity * 2;
+    void **table = calloc(capacity, sizeof(void *));
+    for (int i = 0; i < set->capacity; i++) {
+      if (set->table[i] && set->table[i] != DELETED) {
+        size_t index = hash(set->table[i], set->sizeOfData, capacity);
+        while (table[index]) {
+          index = (index + 1) % capacity;
         }
-        free(set->table);
-        set->table = table;
-        set->capacity = capacity;
+        table[index] = set->table[i];
+      }
     }
-    size_t index = hash(data, set->sizeOfData, set->capacity);
-    while (set->table[index]) {
-        if (memcmp(set->table[index], data, set->sizeOfData) == 0) {
-            return;
-        }
-        index = (index + 1) % set->capacity;
+    free(set->table);
+    set->table = table;
+    set->capacity = capacity;
+  }
+  size_t index = hash(data, set->sizeOfData, set->capacity);
+  while (set->table[index]) {
+    if (set->table[index] == DELETED) {
+      index = (index + 1) % set->capacity;
+      continue;
     }
-    void *item = copy_hashable(data, set->sizeOfData);
-    set->table[index] = item;
-    set->len++;
+    if (memcmp(set->table[index], data, set->sizeOfData) == 0) {
+      return;
+    }
+    index = (index + 1) % set->capacity;
+  }
+  void *item = copy_hashable(data, set->sizeOfData);
+  set->table[index] = item;
+  set->len++;
 }
 
 bool Set_find(const Set *set, void *data) {
-    size_t index = hash(data, set->sizeOfData, set->capacity);
-    while (set->table[index]) {
-        if (memcmp(set->table[index], data, set->sizeOfData) == 0) {
-            return true;
-        }
-        index = (index + 1) % set->capacity;
+  size_t index = hash(data, set->sizeOfData, set->capacity);
+  while (set->table[index]) {
+    if (set->table[index] == DELETED) {
+      index = (index + 1) % set->capacity;
+      continue;
     }
-    return false;
+    if (memcmp(set->table[index], data, set->sizeOfData) == 0) {
+      return true;
+    }
+    index = (index + 1) % set->capacity;
+  }
+  return false;
 }
 
 void **Set_items(const Set *set) {
-    void **items = calloc(set->len, sizeof(void *));
-    assert(items);
-    size_t index = 0;
-    for (int i = 0; i < set->capacity; i++) {
-        if (set->table[i]) {
-            items[index++] = set->table[i];
-        }
+  void **items = calloc(set->len, sizeof(void *));
+  assert(items);
+  size_t index = 0;
+  for (int i = 0; i < set->capacity; i++) {
+    if (set->table[i] && set->table[i] != DELETED) {
+      items[index++] = set->table[i];
     }
-    return items;
+  }
+  return items;
 }
 
 void Set_free(void *set_void) {
-    Set *set = (Set *)set_void;
-    for (int i = 0; i < set->capacity; i++) {
-        if (set->table[i]) {
-            free(set->table[i]);
-        }
+  Set *set = (Set *)set_void;
+  for (int i = 0; i < set->capacity; i++) {
+    if (set->table[i] && set->table[i] != DELETED) {
+      free(set->table[i]);
     }
-    free(set->table);
-    free(set);
+  }
+  free(set->table);
+  free(set);
 }
 
 void *Set_return(const void *src) { return src; }
 
 void *Set_copy(const void *src) {
-    assert(src);
-    Set *srcSet = (Set *)src;
-    Set *destSet = Set_create(srcSet->capacity, srcSet->sizeOfData);
-    for (size_t i = 0; i < srcSet->capacity; i++) {
-        if (srcSet->table[i]) {
-            Set_add(destSet, srcSet->table[i]);
-        }
+  assert(src);
+  Set *srcSet = (Set *)src;
+  Set *destSet = Set_create(srcSet->capacity, srcSet->sizeOfData);
+  for (size_t i = 0; i < srcSet->capacity; i++) {
+    if (srcSet->table[i] && srcSet->table[i] != DELETED) {
+      Set_add(destSet, srcSet->table[i]);
     }
-    return destSet;
+  }
+  return destSet;
+}
+
+void Set_remove(Set *set, const void *data) {
+  size_t index = hash(data, set->sizeOfData, set->capacity);
+  assert(Set_find(set, data));
+  while (set->table[index] && set->table[index] != DELETED) {
+    if (memcmp(set->table[index], data, set->sizeOfData) == 0) {
+      free(set->table[index]);
+      set->table[index] = DELETED;
+      set->len--;
+      return;
+    }
+    index = (index + 1) % set->capacity;
+  }
+  return;
 }
